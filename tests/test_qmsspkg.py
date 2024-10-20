6cf8ad4128dc433205a0988c8846e4d79a3d5ce2
Thomas Brambor <tbrambor@gmail.com>
2024-10-20 00:26:02 -0400

    build: add CI workflow

--- tests/test_qmsspkg.py
+++ tests/test_qmsspkg.py
@@ -1,1 +1,10 @@
 from qmsspkg import qmsspkg
+import pandas as pd
+
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((qmsspkg.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((qmsspkg.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
